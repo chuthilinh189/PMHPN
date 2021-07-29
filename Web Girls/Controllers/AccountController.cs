4e4d5b190c9213d43bf6a34284829b8daeefd3d0
chuthilinh189 <chuthilinh189@gmail.com>
2021-07-29 15:02:44 +0700

    PM HPN

---
+++ Web Girls/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web_Girls.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+          public ActionResult Account_Add()
+          {
+               return View();
+          }
+     }
+}
---
+++ Web Girls/Controllers/MemberController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web_Girls.Controllers
+{
+     public class MemberController : Controller
+     {
+          public ActionResult Index()
+          {
+               return View();
+          }
+          public ActionResult Member_Add()
+          {
+               return View();
+          }
+          public ActionResult Member_Add_Detail()
+          {
+               return View();
+          }
+     }
+}
